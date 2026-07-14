f52f356e54b1b76b8c65e18f102bc1e141f9404a
Matheus Dias <betoneirasegames@gmail.com>
2026-07-14 17:16:04 -0300

    feat: Fazer o import de ListProdutos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; Removendo esse RouterOutlet pois não precisarei agora
 import { Produto } from './components/produto/produto';
+import { ListProdutos } from './components/list-produtos/list-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
