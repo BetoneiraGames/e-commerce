5765f0b8684b59568c7ebd254e21f69161c881ab
Matheus Dias <betoneirasegames@gmail.com>
2026-07-14 14:48:07 -0300

    feat(list-produtos): cria componente list-produto

---
+++ src/app/components/list-produtos/list-produtos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-produtos',
+  imports: [],
+  templateUrl: './list-produtos.html',
+  styleUrl: './list-produtos.css',
+})
+export class ListProdutos {}
