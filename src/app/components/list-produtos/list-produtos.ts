c109133b444883fe60422af2d4738716cb5013e2
liliani mendes <mendessantos0023@gmail.com>
2026-07-15 13:47:25 -0300

    refactor: Colocado a lista de produtos para mostrar

--- src/app/components/list-produtos/list-produtos.ts
+++ src/app/components/list-produtos/list-produtos.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Produto } from '../produto/produto'
 @Component({
   selector: 'app-list-produtos',
-  imports: [],
+  imports: [Produto],
   templateUrl: './list-produtos.html',
   styleUrl: './list-produtos.css',
 })
-export class ListProdutos {}
+export class ListProdutos {
+  produtos = [
+    {nome: 'Teclado', preco:89.98},
+    {nome: 'Mouse Gamer', preco:78.90},
+    {nome: 'Monitor', preco:849.99},
+    {nome: 'Mesa', preco:450.59},
+    {nome: 'Headset', preco:99.59}
+  ];
+}
