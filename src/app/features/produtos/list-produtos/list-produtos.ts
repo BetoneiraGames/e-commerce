dc24efe2f1bf6a351c49a8f84f2dc07052ff4b15
Matheus Dias <betoneirasegames@gmail.com>
2026-07-16 15:43:57 -0300

    refactor: atualizaproduto

--- src/app/features/produtos/list-produtos/list-produtos.ts
+++ src/app/features/produtos/list-produtos/list-produtos.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
 @Component({
   selector: 'app-list-produtos',
@@ -7,14 +7,24 @@ import { Produto } from '../produto/produto';
   styleUrl: './list-produtos.css',
 })
 export class ListProdutos {
-  produtos = [
+  produtos = signal([
     {nome: 'Teclado', preco:89.98},
     {nome: 'Mouse Gamer', preco:78.90},
     {nome: 'Monitor', preco:849.99},
     {nome: 'Mesa', preco:450.59},
     {nome: 'Headset', preco:99.59}
-  ];
+  ]);
 exibirProduto(nome:string){
   console.log('Produto selecionado: ', nome);
 }
+ adicionarProduto(){
+  this.produtos.update(listaAtual => [
+    ...listaAtual,
+     {nome:'Polystation', preco:1099.99},
+     {nome:'Playstation', preco:599.90}
+
+  ]);
+
+ }
+
 }
