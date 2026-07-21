347890900b998223fa07ff99e11780b17db4604a
Matheus Dias <betoneirasegames@gmail.com>
2026-07-21 13:55:06 -0300

    feat: cria import carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ListProdutos } from './features/produtos/list-produtos/list-produtos';
+import { Carrinho } from './features/carrinho/carrinho/carrinho';
+export const routes: Routes = [
+  { 
+    path: '', component: ListProdutos 
+},
+  { 
+    path: 'carrinho', component: Carrinho 
+}
+];
