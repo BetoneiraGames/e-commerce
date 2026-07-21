b3ae926871e485fd399d62f2967f6ea055ee147a
Matheus Dias <betoneirasegames@gmail.com>
2026-07-21 13:56:51 -0300

    feat: fazer o import do Router e criar variável nomeLoja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router'; Removendo esse RouterOutlet pois não precisarei agora
-import { Produto } from './features/produtos/produto/produto';
-import { ListProdutos } from './features/produtos/list-produtos/list-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+//import { Produto } from './features/produtos/produto/produto';
+//import { ListProdutos } from './features/produtos/list-produtos/list-produtos';
 @Component({
   selector: 'app-root',
-  imports: [ListProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Feira do Rolo'; //nome do e-commerce
 }
