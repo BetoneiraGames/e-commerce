7b15d9d11cdcb241a9ee7de9bb4ee3aceb2afe6e
Matheus Dias <betoneirasegames@gmail.com>
2026-07-22 16:15:31 -0300

    feat: criação de uma pasta com arquivos e criação da autenticação

---
+++ src/app/core/auth.ts
@@ -0,0 +1,13 @@
+import { signal } from "@angular/core";
+//! Define valor inicial do signal usuarioLogado com (false)
+export const usuarioLogado = signal (false);
+
+//! Define Signal usuarioLogado como (true), Permite acesso as rotas
+export function login (){
+    usuarioLogado.set(true);
+}
+
+//! Define Signal usuarioLogado como (false), Bloqueia o acesso imediatamente
+    export function logout(){
+        usuarioLogado.set(false);
+    }
