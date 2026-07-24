0b74b4b9a7db2af8807a3ad3ce47589e1073d33c
Matheus Dias <betoneirasegames@gmail.com>
2026-07-24 14:07:16 -0300

    fix: algumas correções

--- src/app/features/produtos/list-produtos/list-produtos.ts
+++ src/app/features/produtos/list-produtos/list-produtos.ts
@@ -5,20 +5,42 @@ import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
+import { produtosService } from '../produtos.service'
+import { inject } from '@angular/core'
 @Component({
   selector: 'app-list-produtos',
-  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
+  imports: [Produto, UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './list-produtos.html',
   styleUrl: './list-produtos.css',
 })
 export class ListProdutos {
-  produtos = signal([
-    {nome: 'Teclado Gamer', preco:149.00},
-    {nome: 'Mouse Gamer', preco:299.99},
-    {nome: 'Monitor Gamer', preco:1599.99},
-    {nome: 'Desktop Gamer', preco:4999.99},
-    {nome: 'Headset Gamer', preco:699.99}
-  ]);
+  //! remover a lista de produtos, dados carregados via API Fakestoreapi
+  produtos = signal <
+  {nome: string ; preco: number } []> ([]);
+//? criar estado de carregamento
+// ** true: requisição em andamento, exibir dados no template
+//! false: esconder indicador e exibir a lista de produtos 
+  carregando = signal(true);
+  //! cria método para a requisição dos produtos 
+  carregarProdutos(){
+    this.carregando.set(true);
+
+    this.produtoService.buscarProduto().subscribe({
+      next: (dados) => {
+        const produtos = this.produtoService.transformarProdutos(dados);
+        this.produtos.set(produtos);
+        this.carregando.set(false);
+      },
+        error: (erro) => {
+          console.error('Erro ao carregar os produtos: ', erro);
+          this.carregando.set(false);
+        
+      },
+    });
+  }
+
+  //? MEtodos existentes Ñ alterar
+
 exibirProduto(nome:string){
   //console.log('Produto selecionado: ', nome);
   this.produtoSelecionado.set(nome);
@@ -44,7 +66,14 @@ substituirProdutos(){
     {nome:'Headset', preco:25}
   ]);
 }
+//! injetar httpCliente dentro de construct, reestruturar construct !!!
+//? METODO HTTP (API) FOI MODIFICADO PARA (ProdutosService)
  constructor(){
+  //!Carregar a API
+  this.carregarProdutos();
+  
+
+  //! efects continuam iguais
   effect(() =>{
     console.log('Lista de produtos alterados: ', this.produtos());
   });
@@ -64,6 +93,9 @@ substituirProdutos(){
     ...listaAtual,produto
   ]);
   }
+
+  private produtoService = inject(produtosService);
+
   quantidadeCarrinho = computed(() => this.carrinho().length);
   totalCarrinho = computed(() => { 
     return this.carrinho().reduce((total, item) =>
